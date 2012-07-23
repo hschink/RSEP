20f6007e1adee5642ae09f40b286d27d5295e3d8
Hagen Schink <hschink@st.ovgu.de>
2012-07-23 18:56:14 +0200

    Added copyright notes to source files of graph semantics plug-in

--- GraphSemanticsPlugin/src/org/iti/rsbp/extension/rs/gbsp/bindinginformation/method/call/MethodInvocationBindingInformation.java
+++ GraphSemanticsPlugin/src/org/iti/rsbp/extension/rs/gbsp/bindinginformation/method/call/MethodInvocationBindingInformation.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Hagen Schink.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Hagen Schink - initial API and implementation
+ ******************************************************************************/
 package org.iti.rsbp.extension.rs.gbsp.bindinginformation.method.call;
 
 import java.util.List;
